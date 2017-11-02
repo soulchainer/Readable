02b110064397cd49c892bb52803ff866613ef0f6
Juan Riquelme González <j.riquelme@medlabmg.com>
2017-11-02 17:27:22 +0100

    feat: add create-action util&initial actions file

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+import createAction from '../utils/create-action';
+// Action types
+export const ACTION_EXAMPLE = 'ACTION_EXAMPLE';
+
+// Action creators
+export const actionCreatorExample = createAction(ACTION_EXAMPLE);
