e48218ff24cde43aa69fc6a400600d011a5502e3
Juan Riquelme González <j.riquelme@medlabmg.com>
2017-11-07 17:29:31 +0100

    feat: add reducers (WIP 2)

--- src/state/ducks/comments/reducers.js
+++ src/state/ducks/comments/reducers.js
@@ -16,24 +16,43 @@ const initialState = {
 };
 
 const commentsReducer = createReducer(initialState)({
-  [types.ADD]: (state, action) => ({
-    ...initialState,
-    comments: action.payload,
+  [types.ADD]: (state, { payload }) => ({
+    ...state,
+    comments: payload,
   }),
-  [types.CHANGE_SORTING_DIRECTION]: (state, action) => ({
-    ...initialState,
-    sortingDirection: action.payload,
+  [types.CHANGE_SORTING_DIRECTION]: (state, { payload }) => ({
+    ...state,
+    sortingDirection: payload,
   }),
-  [types.CHANGE_SORTING_METHOD]: (state, action) => ({
-    ...initialState,
-    sortingMethod: action.payload,
+  [types.CHANGE_SORTING_METHOD]: (state, { payload }) => ({
+    ...state,
+    sortingMethod: payload,
   }),
-  [types.DELETE]: (state, action) => ({
-    ...initialState,
-    comments: initialState.comment.filter((comment) => {
-      return comment.id !== action.payload;
+  [types.DELETE]: (state, { payload }) => ({
+    ...state,
+    comments: state.comments.filter(({ id }) => {
+      return id !== payload;
     }),
   }),
+  [types.EDIT]: (state, { payload }) => ({
+    ...state,
+    comments: [
+      ...state.comments.filter(({ id }) => comment.id !== id),
+      payload,
+    ],
+  }),
+  [types.GET]: (state, { payload }) => ({
+    ...state,
+    comments: payload,
+  }),
+  [types.UPDATE_VOTE_SCORE]: (state, { payload }) => ({
+    ...state,
+    comments: state.comments.map((comment) => {
+      // if different id, return the same
+      // if the same id, update the score accordingly
+      return 
+    } 
+  })
 });
 
 export default commentsReducer;
