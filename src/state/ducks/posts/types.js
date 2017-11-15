ef597fdd183f5b478d8689c447062b92e80c2f85
Juan Riquelme González <juanriqgon@protonmail.com>
2017-11-16 00:34:32 +0100

    feat: change posts (action) types (WIP5)

--- src/state/ducks/posts/types.js
+++ src/state/ducks/posts/types.js
@@ -1,9 +1,55 @@
 // Action types
-export const ADD = 'posts/ADD';
+
+/**
+ * Dispatched after post were succesfully added on the server
+ */
+export const ADDED = 'posts/ADDED';
+/**
+ * Dispatched when an API call to add a post is sent
+ */
+export const ADDING = 'posts/ADDING';
+/**
+ * Dispatched when an API call to add post fails
+ */
+export const ADD_ERROR = 'posts/ADD_ERROR';
 export const CHANGE_SORTING_DIRECTION = 'posts/CHANGE_SORTING_DIRECTION';
 export const CHANGE_SORTING_METHOD = 'posts/CHANGE_SORTING_METHOD';
-export const DELETE = 'posts/DELETE';
-export const EDIT = 'posts/EDIT';
+/**
+ * Dispatched after post were succesfully deleted on the server
+ */
+export const DELETED = 'posts/DELETED';
+/**
+ * Dispatched when an API call to delete a post is sent
+ */
+export const DELETING = 'posts/DELETING';
+/**
+ * Dispatched when an API call to delete a post fails
+ */
+export const DELETE_ERROR = 'posts/DELETE_ERROR';
+/**
+ * Dispatched after post were succesfully edited on the server
+ */
+export const EDITED = 'posts/EDITED';
+/**
+ * Dispatched when an API call to edit a post is sent
+ */
+export const EDITING = 'posts/EDITING';
+/**
+ * Dispatched when an API call to edit a post fails
+ */
+export const EDIT_ERROR = 'posts/EDIT_ERROR';
+/**
+ * Dispatched after posts were succesfully fetched from the server
+ */
+export const FETCHED = 'posts/FETCHED';
+/**
+ * Dispatched when an API call to fetch all posts fails
+ */
+export const FETCH_ERROR = 'posts/FETCH_ERROR';
+/**
+ * Dispatched when an API call to fetch all posts is sent
+ */
+export const LOADING = 'posts/LOADING';
+
 export const GET_FROM_CATEGORY = 'posts/GET_FROM_CATEGORY';
-export const GET = 'posts/GET';
 export const UPDATE_VOTE_SCORE = 'posts/UPDATE_VOTE_SCORE';
