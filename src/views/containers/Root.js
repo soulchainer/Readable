7ed3f66dcf2b3a667db6f36aea23328999863201
Juan Riquelme González <juanriqgon@protonmail.com>
2017-11-19 13:47:02 +0100

    refactor: remove the extra Root.env containers
    Now we use dynamic ES6 `import()` to import one or another
    component depending of the current environment.
    This way, we also handle the Root in just one file, less code and
    also a short file.
    
    I also add here a `<App>` container. This component will host the
    app routes and also will do the main fetching of data of the app
    (data that need to be shared between several components). And,
    obviously, that data fetched will be stored into Redux store, for
    being required from everywhere inside the app.

--- src/views/containers/Root.js
+++ src/views/containers/Root.js
@@ -1,7 +1,40 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./Root.prod');
-} else {
-  module.exports = require('./Root.dev');
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import App from './App';
+
+class Root extends Component {
+  state = { devtools: null };
+
+  /**
+   * Do conditional imports when component is mounted.
+   */
+  componentDidMount() {
+    const { NODE_ENV: env } = process.env;
+    const isDev = env === 'development';
+    if (isDev) {
+      import('./DevTools')
+        .then(module => this.setState({ devtools: module.default }));
+    }
+  }
+
+  render() {
+    const { store } = this.props;
+    const { devtools: DevTools } = this.state;
+    return (
+      <Provider store={store}>
+        <div>
+          {DevTools && <DevTools />}
+          <App />
+        </div>
+      </Provider>
+    );
+  }
 }
-/* eslint-enable global-require */
+
+Root.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  store: PropTypes.object.isRequired,
+};
+
+export default Root;
