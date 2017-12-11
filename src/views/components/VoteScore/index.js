af03b948338d8039c1ee1e410ca5f4cbd333ad36
Juan Riquelme González <j.riquelme@medlabmg.com>
2017-12-11 17:30:01 +0100

    feat: add ops to vote & VoteScore component (WIP2)

--- src/views/components/VoteScore/index.js
+++ src/views/components/VoteScore/index.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 
 
 class VoteScore extends Component {
+  state = {
+    votingDisabled: false,
+  }
+
   componentWillReceiveProps(nextProps) {
     const {
       voteState: {
@@ -16,7 +20,31 @@ class VoteScore extends Component {
      * show error messages/console messages if some problem occured.
      * Also apply this component to posts. Add an state for disable the buttons.
      */
+
+     /**
+     * TODO: Improve this. Add a loading message, maybe with a spinner,
+     * notifying there is an edit/new comment addition in progress, via console?
+     * Define the `toggleVotingDisabled` function.
+     */
+    const isUpdatingScoreChanged = isUpdatingScore !== nextProps.isUpdatingScore;
+    const updateScoreEnd = isUpdatingScoreChanged && !nextProps.isUpdatingScore;
+
+    if (isUpdatingScoreChanged) {
+      this.setState(this.toggleVotingDisabled);
+    }
+    if (isUpdatingScoreChanged) {
+      if (updateScoreEnd) {
+        if (nextProps.updateScoreHasFailed) {
+          /* The voteScore update failed */
+          this.setState({
+            errorMessage: 'There was an error updating the comment score',
+          });
+        }
+        /* The voteScore was updated succesfully */
+      }
+    }
   }
+
   downVote = () => this.props.updateScore('downVote');
   upvote = () => this.props.updateScore('upVote');
   render() {
