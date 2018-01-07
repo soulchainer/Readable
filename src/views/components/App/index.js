8ed4ffadb6d730ab280135396342edbb4794abd6
Juan Riquelme González <juanriqgon@protonmail.com>
2018-01-08 00:14:53 +0100

    refactor: extract glob CSS in App to separate file

--- src/views/components/App/index.js
+++ src/views/components/App/index.js
@@ -5,6 +5,7 @@ import {
   Switch,
 } from 'react-router-dom';
 import routes from 'routes';
+import './styles.css';
 
 const App = () => (
   <Router>
@@ -12,64 +13,6 @@ const App = () => (
       <Switch>
         {routes.map(args => <Route key={args.path} {...args} />)}
       </Switch>
-      <style jsx global>
-        {`
-          * {
-            box-sizing: border-box;
-          }
-
-          body {
-            margin: 0;
-            max-width: 100%;
-            min-height: 100vh;
-            overflow-x: hidden;
-            width: 100vw;
-          }
-
-          /* For this quick input CSS reset, I look at https://codepen.io/terkel/pen/dvejH */
-          input[type="submit"],
-          input[type="reset"] {
-            background: none;
-            border: 0;
-            color: inherit;
-            font: inherit;
-            line-height: normal;
-            overflow: visible;
-            padding: 0;
-            -webkit-appearance: button; /* for input */
-              -moz-user-select: none;
-                -ms-user-select: none;
-          }
-          input::-moz-focus-inner {
-            border: 0;
-            padding: 0;
-          }
-
-          #root {
-            display: flex;
-            justify-content: center;
-            min-height: 100vh;
-          }
-
-          .Root {
-            display: flex;
-            font-family: 'Exo', sans-serif;
-            height: 100%;
-            justify-content: center;
-            width: 100vw;
-          }
-
-          .Screen {
-            display: flex;
-            flex-direction: column;
-            width: 100vw;
-          }
-
-          .icon {
-            height: 100%;
-          }
-        `}
-      </style>
     </div>
   </Router>
 );
